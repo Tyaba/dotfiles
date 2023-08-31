584905045a839e77a5863650a28a1d42edb5657e
Tyaba <tepei@keio.jp>
2023-08-31 11:08:44 +0900

    MOD: themes

--- config/.zsh/lib/aliases.zsh
+++ config/.zsh/lib/aliases.zsh
@@ -5,19 +5,27 @@ alias ....="cd ../../.."
 alias g="git"
 alias s="git status -sb"
 
+# replace
+alias git="hub"
 # bat replaces cat
 alias cat="bat"
+# rip grep replaces grep
+alias grep="rg"
 # exa replaces ls
 alias ls="exa -a"
 # procs replaces ps
 alias ps="procs"
+# fd replaces find
+alias find="fd"
+# bottom replaces top
+alias top="btm"
+
+# in short
 # lazydocker
 alias lzd="lazydocker"
 # kubernetes
 source <(kubectl completion zsh)
 alias k=kubectl
-# fd replaces find
-alias find="fd"
 # terraform
 alias tf="terraform"
 # gcloud
@@ -26,7 +34,5 @@ alias gcurl='curl --header "Authorization: Bearer $(gcloud auth print-identity-t
 alias d=docker
 # dust replaces du
 alias du="dust"
-# bottom replaces top
-alias top="btm"
 # cargo-script
 alias rust="cargo-script"
