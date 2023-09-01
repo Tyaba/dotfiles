08aaa53396ebfd4438afc183fe8c575e1af63c96
Tyaba <tepei@keio.jp>
2023-09-01 23:40:06 +0900

    ADD: locales-allを通すためにadd-apt-repositoryを通すためにlaunchpad-getkeysを入れた

--- cookbooks/basic/default.rb
+++ cookbooks/basic/default.rb
@@ -2,10 +2,21 @@ package 'software-properties-common'
 
 # locale-gen
 package 'locales'
-case node[:platform]
-when 'ubuntu', 'debian'
-  execute 'sudo add-apt-repository -y universe && sudo apt-get update'
+execute 'locale-gen-setup' do
+  case node[:platform]
+  when 'ubuntu', 'debian'
+    command "
+    sudo add-apt-repository ppa:nilarimogard/webupd8 &&
+    sudo apt-get update &&
+    sudo apt-get install launchpad-getkeys &&
+    sudo apt-get update &&
+    sudo add-apt-repository -y universe &&
+    sudo apt-get update
+    "
+  end
+  not_if "locale | grep en_US.UTF-8"
 end
+
 package 'locales-all'
 execute 'sudo locale-gen en_US.UTF-8' do
   not_if "locale | grep en_US.UTF-8"
