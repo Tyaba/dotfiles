ec3fd2f9a3095e638a48fec2f624add6ebba9fee
Tyaba <tepei@keio.jp>
2023-09-02 00:04:17 +0900

    codenameをnodeに入れる

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -35,8 +35,8 @@ when 'ubuntu', 'debian'
   curl -fsSL https://download.docker.com/linux/#{node[:platform]}/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
   sudo chmod a+r /etc/apt/keyrings/docker.gpg &&
   echo \
-  'deb [arch='$(dpkg --print-architecture)' signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/#{node[:platform]} \
-  '$(. /etc/os-release && echo '$(echo $VERSION_CODENAME)')' stable' | \
+  'deb [arch='$(dpkg --print-architecture)' signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  '$(. /etc/os-release && echo #{node[:codename]})' stable' | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
   sudo apt-get update &&
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,5 +2,7 @@ include_recipe 'recipe_helper'
 
 node.reverse_merge!(
   user: ENV['SUDO_USER'] || ENV['USER'],
+  codename: run_command('lsb_release -cs').stdout.strip,
 )
+
 include_role node[:platform]
