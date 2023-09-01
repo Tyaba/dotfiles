82459f670b781f8b985e57c75ade25d8b3df5388
Tyaba <tepei@keio.jp>
2023-09-01 22:54:17 +0900

    ADD: apt install locales-all失敗対策

--- cookbooks/basic/default.rb
+++ cookbooks/basic/default.rb
@@ -1,15 +1,21 @@
-# locale-gen
-package 'locales'
-package 'locales-all'
-execute 'sudo locale-gen en_US.UTF-8' do
-  not_if "locale | grep en_US.UTF-8"
-end
 package 'libffi-dev'
 package 'zlib1g-dev'
 package 'curl'
 package 'openssh-client'
 package 'build-essential'
 package 'zip'
+package 'software-properties-common'
+
+# locale-gen
+package 'locales'
+case node[:platform]
+when 'ubuntu', 'debian'
+  execute 'sudo add-apt-repository -y universe'
+end
+package 'locales-all'
+execute 'sudo locale-gen en_US.UTF-8' do
+  not_if "locale | grep en_US.UTF-8"
+end
 
 # クリップボードコピー
 case node[:platform]
