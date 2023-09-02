714b4e4be06b7d8acebab4af298a1a8359f507b4
Tyaba <tepei@keio.jp>
2023-09-03 02:54:30 +0900

    ADD: comment

--- cookbooks/gpg-agent/default.rb
+++ cookbooks/gpg-agent/default.rb
@@ -1,10 +1,12 @@
+# このレシピは、gpg-agentをインストールし、設定ファイルを配置する
+# gnupg-agentをinstall
 package 'gnupg-agent'
-
+# sudoで作られた~/.gnupgのownerをユーザに変更
 directory "#{ENV['HOME']}/.gnupg" do
     owner node[:user]
     group node[:user]
 end
-
+# files/gpg-agent.confを~/.gnupg/gpg-agent.confに配置
 remote_file "#{ENV['HOME']}/.gnupg/gpg-agent.conf" do
     source 'files/gpg-agent.conf'
     owner node[:user]
--- cookbooks/ssh-agent/default.rb
+++ cookbooks/ssh-agent/default.rb
@@ -1,13 +1,18 @@
+# このレシピは、ssh-agentの設定ファイルを配置する
+# systemdについて：参考URL
+# https://wiki.archlinux.jp/index.php/Systemd/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC
+# ~/.config/systemd/userはユーザが配置する、そのユーザのサービス
 remote_file "#{ENV['HOME']}/.config/systemd/user/ssh-agent.service" do
     source 'files/ssh-agent.service'
     owner node[:user]
     group node[:user]
 end
-
+# systemdのユーザインスタンスの環境変数は.bashrcなどの設定を継承しない。.pam-environmentに設定する
 remote_file "#{ENV['HOME']}/.pam-environment" do
     source 'files/.pam-environment'
 end
 
+# default.target.wantsからユーザ定義サービスへのssh-agent.serviceのシンボリックリンクを作成する
 link "#{ENV['HOME']}/.config/systemd/user/default.target.wants/ssh-agent.service" do
     to "#{ENV['HOME']}/.config/systemd/user/ssh-agent.service"
 end
