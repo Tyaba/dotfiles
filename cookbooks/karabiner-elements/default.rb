5c6c687324bff893bfd995a0dedc4b994301b8ee
Tyaba <tepei@keio.jp>
2024-03-02 00:36:28 +0900

    FIX: grammar

--- cookbooks/karabiner-elements/default.rb
+++ cookbooks/karabiner-elements/default.rb
@@ -4,8 +4,10 @@ when 'darwin'
   # karabiner-elementsの設定ファイルをリンクする
   execute 'mkdir -p $HOME/.config' do
     not_if 'test -d $HOME/.config'
+  end
   execute 'ln -sf $HOME/.dotfiles/config/.config/karabiner/ $HOME/.config/' do
     not_if 'test -d $HOME/.config/karabiner'
+  end
 else
   raise NotImplementedError
 end
