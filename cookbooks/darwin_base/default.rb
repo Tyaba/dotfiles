9211aadc5489e48ef36912a9e3c0ce9b76f9be1b
Tyaba <tepei@keio.jp>
2024-03-02 01:05:19 +0900

    FIX: disable screen saver

--- cookbooks/darwin_base/default.rb
+++ cookbooks/darwin_base/default.rb
@@ -1,7 +1,8 @@
 case node[:platform]
 when 'darwin'
   execute 'disable screen saver' do
-    command 'sudo defaults -currentHost write com.apple.screensaver idleTime 0'
+    command 'defaults -currentHost write com.apple.screensaver idleTime -int 0'
+    not_if 'defaults -currentHost read com.apple.screensaver idleTime | test -w 0'
   end
 else
   raise NotImplementedError
