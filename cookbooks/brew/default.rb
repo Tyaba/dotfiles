53372ad895533863a61381c4f13ea9ffe851879d
Tyaba <tepei@keio.jp>
2024-03-01 17:31:42 +0900

    ADD: brew update

--- cookbooks/brew/default.rb
+++ cookbooks/brew/default.rb
@@ -4,6 +4,8 @@ when 'darwin'
     command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
     not_if 'which brew'
   end
+  execute 'brew update' do
+    command 'brew update'
 else
   raise NotImplementedError
 end
