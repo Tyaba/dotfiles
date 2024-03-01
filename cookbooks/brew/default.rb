8c32c6319143d4e98776dda44280bdfc57d7f939
Tyaba <tepei@keio.jp>
2024-03-01 17:35:51 +0900

    ADD: homebrew path

--- cookbooks/brew/default.rb
+++ cookbooks/brew/default.rb
@@ -1,5 +1,9 @@
 case node[:platform]
 when 'darwin'
+  unless ENV['PATH'].include?("/opt/homebrew/bin")
+    MItamae.logger.info('Prepending /opt/homebrew/bin to PATH during this execution')
+    ENV['PATH'] = "/opt/homebrew/bin:#{ENV['PATH']}"
+  end
   execute 'install brew' do
     command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
     not_if 'which brew'
