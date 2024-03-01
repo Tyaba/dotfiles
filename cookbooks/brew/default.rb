a71c71149e3bbd38494ff3c93d352b1669d323d7
Tyaba <tepei@keio.jp>
2024-03-01 17:24:45 +0900

    FIX: install brew

--- cookbooks/brew/default.rb
+++ cookbooks/brew/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'install brew' do
-    command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
     not_if 'which brew'
   end
 else
