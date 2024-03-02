a5440b7cd23483095d5333ca84028840cbf520b4
Tyaba <tepei@keio.jp>
2024-03-02 23:15:40 +0900

    FIX: install bitwarden with app store instead of brew

--- cookbooks/bitwarden/default.rb
+++ cookbooks/bitwarden/default.rb
@@ -1,6 +1,8 @@
 case node[:platform]
 when 'darwin'
-  execute 'brew install --cask bitwarden' do
+  # brewで入れた場合はブラウザ統合が使えないので、App Storeから入れる
+  execute 'install bitwarden' do
+    command 'mas install 1352778147'
     not_if 'test -d /Applications/Bitwarden.app'
   end
 else
