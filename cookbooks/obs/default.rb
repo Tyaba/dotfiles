6d969fc82532439b7e628c8fa53f2fb584f53108
Tyaba <tepei@keio.jp>
2024-03-05 13:28:09 +0900

    FIX: obs

--- cookbooks/obs/default.rb
+++ cookbooks/obs/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when "darwin"
-    command "brew install --cask obs" do
-        not_if "test -d /Applications/OBS.app"
+    execute "brew install --cask obs" do
+    not_if "test -d /Applications/OBS.app"
 end
 else
     raise NotImplementedError
