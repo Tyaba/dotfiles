d8450125287d39e3ee9684f337d3797050cfdced
Tyaba <tepei@keio.jp>
2024-03-08 00:27:25 +0900

    FIX: ヒアドキュメント

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,5 +1,9 @@
 execute 'install ghq' do
-    command 'asdf plugin add ghq && asdf install ghq latest && asdf global ghq latest'
+    command <<-EOF
+        asdf plugin add ghq
+        asdf install ghq latest
+        asdf global ghq latest
+    EOF
     not_if 'asdf plugin list | grep ghq'
     # $REMOTE_CONTAINERがtrueの場合は実行しない
     not_if 'echo $REMOTE_CONTAINER'
