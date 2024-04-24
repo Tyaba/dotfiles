a68a48382d33e3782d52e9c6a442362033cec451
Tyaba <tepei@keio.jp>
2024-04-24 15:02:00 +0000

    fix: mise install ghq

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,5 +1,6 @@
 execute 'install ghq' do
     command <<-EOF
+        mise plugin install ghq https://github.com/kajisha/asdf-ghq.git
         mise install ghq@latest
         mise use --global ghq@latest
     EOF
