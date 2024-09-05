c6d22144f27a2c5da5d5bfa866d4210bb5d1b594
Tyaba <tepei@keio.jp>
2024-09-05 10:07:16 +0000

    doc(golang): add todo

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,5 +1,7 @@
-package "golang"
+package "go"
+# TODO: golangとdlvのversion compatibilityの問題でdlv latestは取れない。
+# golangをversion指定して取るか、
 execute "get dlv" do
-    command "go install -v github.com/go-delve/delve/cmd/dlv@latest"
+    command "go install -v github.com/go-delve/delve/cmd/dlv@v1.23.0"
     not_if 'which dlv'
 end
