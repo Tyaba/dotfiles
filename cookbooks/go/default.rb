1c38f0d3228b0c2897c4d63b7dd5ad5151109cd3
Tyaba <tepei@keio.jp>
2024-09-13 06:12:55 +0000

    fix: go

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,7 +1,15 @@
-package "go"
+case node['platform']
+when 'debian', 'ubuntu'
+    package "golang"
+when 'mac_os_x'
+    package "go"
+else
+    raise NotImplementedError
+end
+
 # TODO: golangとdlvのversion compatibilityの問題でdlv latestは取れない。
 # golangをversion指定して取るか、
-execute "get dlv" do
-    command "go install -v github.com/go-delve/delve/cmd/dlv@v1.23.0"
-    not_if 'which dlv'
-end
+# execute "get dlv" do
+#     command "go install -v github.com/go-delve/delve/cmd/dlv@v1.23.0"
+#     not_if 'which dlv'
+# end
