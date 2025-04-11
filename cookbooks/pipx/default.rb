1a540ad3bf0b997a70a9f15d032c91f098fa6bc8
Tyaba <tepei@keio.jp>
2025-04-11 06:42:31 +0000

    feat: GCE対応

--- cookbooks/pipx/default.rb
+++ cookbooks/pipx/default.rb
@@ -1,3 +1,9 @@
+# PATHに/opt/python/3.10/bin/を追加する. Compute Engine用
+unless ENV['PATH'].include?("/opt/python/3.10/bin")
+  MItamae.logger.info('Prepending /opt/python/3.10/bin to PATH during this execution')
+  ENV['PATH'] = "/opt/python/3.10/bin:#{ENV['PATH']}"
+end
+
 execute "install pipx" do
   command "python -m pip install --user --upgrade pipx"
   not_if 'which pipx'
