5e9f56e82eb5e848ed6139f6680f03f5a3e0742a
Tyaba <tepei@keio.jp>
2026-01-08 09:26:39 +0000

    feat: xk6-dashboard -> k6

--- cookbooks/k6/default.rb
+++ cookbooks/k6/default.rb
@@ -2,14 +2,17 @@ case node[:platform]
 when 'darwin'
     package "k6"
 when 'ubuntu', 'debian'
+    package 'gnupg'
+    package 'curl'
     execute "install k6" do
         command <<-EOF
-            mkdir -p /tmp/k6
-            wget -O /tmp/k6/k6.tar.gz https://github.com/grafana/xk6-dashboard/releases/download/v0.7.4/xk6-dashboard_v0.7.4_linux_amd64.tar.gz
-            tar -xzf /tmp/k6/k6.tar.gz -C /tmp/k6
-            mv /tmp/k6/k6 /usr/bin/k6
+            sudo rm -f /usr/share/keyrings/k6-archive-keyring.gpg
+            curl -fsSL https://dl.k6.io/key.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/k6-archive-keyring.gpg > /dev/null
+            echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+            sudo apt-get update
+            sudo apt-get install k6 -y
         EOF
-        not_if "which k6"
+        not_if "dpkg -s k6"
     end
 else
     raise "Not supported platform: #{node[:platform]}"
