2fd7b91c9b412e5cfa5a9055325f7c7330939bf3
Tyaba <tepei@keio.jp>
2026-02-01 12:02:42 +0900

    feat: add cursor cli

--- cookbooks/cursor/default.rb
+++ cookbooks/cursor/default.rb
@@ -3,6 +3,10 @@ when 'darwin'
   execute 'brew install --cask cursor' do
     not_if 'which cursor'
   end
+
+  execute 'brew install --cask cursor-cli' do
+    not_if 'which agent'
+  end
 else
   raise NotImplementedError
 end
